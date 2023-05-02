11ba812ed55e645a4e6ff57e53c7234f18c8857f
unknown <aboisen@tktk.ee>
2023-05-02 19:35:46 +0300

    create new app

---
+++ Myfirstapp/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information. This is a comment to my code.
+
+// rakendus küsib kasutajal sisestada kasutaja tema nime ja see järel rakendus tervitab kasutajat nime pidi
+
+Console.WriteLine("Hello, Auli!");
+Console.WriteLine("enter your name:"); //output
+
+//string-sõne
+string userName = Console.ReadLine(); //input
+
+Console.WriteLine("Hello" + "," + userName + "!"); //output
+
+//string interpolatsion
+Console.WriteLine($"Hello, {userName}!");//output
